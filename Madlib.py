531e6d322ca3924fbb151d107b989b4d686b5362
Rayan Haroon <110626915+1rayanharoon@users.noreply.github.com>
2024-07-30 09:13:12 -0700

    chore: Add Madlib.py script for generating personalized messages

---
+++ Madlib.py
@@ -0,0 +1,12 @@
+# what is a madlib?
+# A madlib is a word game that takes user input and places it into a pre-written story.
+# The result is usually funny or nonsensical.
+
+name=input("Enter your name: ")
+programmin_language=input("Enter the programming language you are learning: ")
+resource=input("Enter the resource you are learning from: ")
+
+
+madlib=(f"Hello my name is {name}. I curretly learnning {programmin_language} from {resource}. ")
+
+print(madlib)
